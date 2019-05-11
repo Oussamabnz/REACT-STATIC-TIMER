289883eea72c08a6af32931b7b04fa5009b3e210
Oussamabnz <oussamabenzineb2@gmail.com>
2019-05-11 17:50:37 +0100

    Seconde Commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Clock = () => {
+  return <div>
+    <center><span>00</span>:<span>30</span>:<span>00</span></center>
+    <center><span>Hour</span><span>Minute</span><span>Second</span></center>
+  </div>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Clock/>
+
   );
 }
 
